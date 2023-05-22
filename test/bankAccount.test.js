50ebbeb2e2764898911f0d6b60845849510d72ff
Quddus Rahman <qrahman115@gmail.com>
2023-05-22 16:58:04 +0100

    refactored test suite to make it more readable and prepare for testing of next method

--- test/bankAccount.test.js
+++ test/bankAccount.test.js
@@ -1,25 +1,24 @@
 const BankAccount = require('../src/bankAccount');
 
 describe('BankAccount', () => {
-  it('should increase the balance when deposit is made', () => {
-    const account = new BankAccount();
+  describe('deposit', () => {
+    test('increases the balance when a valid positive amount is deposited', () => {
+      const account = new BankAccount();
+      const depositAmount = 1000;
+      account.deposit(depositAmount);
+      expect(account.balance).toEqual(depositAmount);
+    });
 
-    expect(account.balance).toEqual(0);
+    test('throws an error if a negative amount is deposited', () => {
+      const account = new BankAccount();
+      const invalidDepositAmount = -1000;
+      expect(() => account.deposit(invalidDepositAmount)).toThrow('Invalid deposit amount');
+    });
 
-    account.deposit(1000);
-
-    expect(account.balance).toEqual(1000);
-  });
-
-  it('should throw an error if a negative amount is deposited', () => {
-    const account = new BankAccount();
-
-    expect(() => account.deposit(-1000)).toThrow('Invalid deposit amount');
-  });
-
-  it('should throw an error if deposit amount is not a number', () => {
-    const account = new BankAccount();
-    
-    expect(() => account.deposit('String')).toThrow('Invalid deposit amount');
+    test('throws an error if deposit amount is not a number', () => {
+      const account = new BankAccount();
+      const invalidDepositAmount = 'String';
+      expect(() => account.deposit(invalidDepositAmount)).toThrow('Invalid deposit amount');
+    });
   });
 });
