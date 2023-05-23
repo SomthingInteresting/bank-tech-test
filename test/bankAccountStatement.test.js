1e57be1a78fd984ea2cba6fe103bec04b75d9118
Quddus Rahman <qrahman115@gmail.com>
2023-05-23 16:04:52 +0100

    added feature test for withdrawal

--- test/bankAccountStatement.test.js
+++ test/bankAccountStatement.test.js
@@ -13,4 +13,11 @@ describe('BankAccount and Statement', () => {
     expect(account.balance).toBe(1000);
     expect(account.transactions.length).toBe(1);
   });
+
+  test('Withdrawal adds a transaction and updates balance', () => {
+    account.deposit(1000);
+    account.withdraw(500);
+    expect(account.balance).toBe(500);
+    expect(account.transactions.length).toBe(2);
+  });
 });
