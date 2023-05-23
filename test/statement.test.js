2c356bdd756b53a68136a35b696119b03c8fdba5
Quddus Rahman <qrahman115@gmail.com>
2023-05-23 15:02:17 +0100

    refactored Statement test suite to make it more readable and identify the dates of mocked transactions better

--- test/statement.test.js
+++ test/statement.test.js
@@ -1,8 +1,12 @@
 const Statement = require('../src/statement');
 const Transaction = require('../src/transaction');
 
+const SAME_DAY_DATE = '2023-01-10T00:00:00Z';
+const NEXT_DAY_DATE = '2023-01-11T00:00:00Z';
+const DAY_AFTER_NEXT_DATE = '2023-01-12T00:00:00Z';
+
 jest.mock('../src/transaction', () => {
-  let mockDate = '2023-01-10T00:00:00Z';
+  let mockDate = SAME_DAY_DATE;
 
   return jest.fn().mockImplementation((amount, type, balance, date) => {
     const transactionDate = date || mockDate;
@@ -24,12 +28,11 @@ jest.mock('../src/transaction', () => {
 describe('Statement', () => {
   describe('format', () => {
     test('formats the transactions for the statement', () => {
-      const mockTransactions = [
-        new Transaction(1000, 'credit', 1000),
-        new Transaction(500, 'debit', 500)
-      ];
+      const transaction1 = new Transaction(1000, 'credit', 1000);
+      const transaction2 = new Transaction(500, 'debit', 500);
+      const sameDayTransactions = [transaction1, transaction2];
 
-      const statement = new Statement(mockTransactions);
+      const statement = new Statement(sameDayTransactions);
       const expectedStatement = 
         'date || credit || debit || balance\n' +
         '10/01/2023 || || 500.00 || 500.00\n' +
@@ -39,15 +42,15 @@ describe('Statement', () => {
     });
 
     test('returns a statement with only the header for no transactions', () => {
-      const mockTransactions = [];
-      const statement = new Statement(mockTransactions);
+      const noTransactions = [];
+      const statement = new Statement(noTransactions);
       const expectedStatement = 'date || credit || debit || balance';
       expect(statement.format()).toEqual(expectedStatement);
     });
 
     test('correctly formats a single transaction', () => {
-      const mockTransactions = [new Transaction(1000, 'credit', 1000)];
-      const statement = new Statement(mockTransactions);
+      const singleTransaction = [new Transaction(1000, 'credit', 1000)];
+      const statement = new Statement(singleTransaction);
       const expectedStatement = 
         'date || credit || debit || balance\n' +
         '10/01/2023 || 1000.00 || || 1000.00';
@@ -55,12 +58,12 @@ describe('Statement', () => {
     });
 
     test('correctly orders and formats multiple transactions on the same day', () => {
-      const mockTransactions = [
-        new Transaction(1000, 'credit', 1000),
-        new Transaction(500, 'debit', 500),
-        new Transaction(500, 'credit', 1000),
-      ];
-      const statement = new Statement(mockTransactions);
+      const transaction1 = new Transaction(1000, 'credit', 1000);
+      const transaction2 = new Transaction(500, 'debit', 500);
+      const transaction3 = new Transaction(500, 'credit', 1000);
+      const sameDayTransactions = [transaction1, transaction2, transaction3];
+
+      const statement = new Statement(sameDayTransactions);
       const expectedStatement = 
         'date || credit || debit || balance\n' +
         '10/01/2023 || 500.00 || || 1000.00\n' +
@@ -70,18 +73,18 @@ describe('Statement', () => {
     });
 
     test('correctly orders and formats transactions on different days', () => {
-      const mockTransactions = [
-        new Transaction(1000, 'credit', 1000, '2023-01-11T00:00:00Z'),
-        new Transaction(500, 'debit', 500),
-        new Transaction(500, 'credit', 1000, '2023-01-12T00:00:00Z'),
-      ];
-      const statement = new Statement(mockTransactions);
+      const transactionDay1 = new Transaction(500, 'debit', 500);
+      const transactionDay2 = new Transaction(1000, 'credit', 1000, NEXT_DAY_DATE);
+      const transactionDay3 = new Transaction(500, 'credit', 1000, DAY_AFTER_NEXT_DATE);
+      const differentDayTransactions = [transactionDay1, transactionDay2, transactionDay3];
+    
+      const statement = new Statement(differentDayTransactions);
       const expectedStatement = 
         'date || credit || debit || balance\n' +
         '12/01/2023 || 500.00 || || 1000.00\n' +
         '11/01/2023 || 1000.00 || || 1000.00\n' +
         '10/01/2023 || || 500.00 || 500.00';
       expect(statement.format()).toEqual(expectedStatement);
-    });
+    });    
   });
 });
