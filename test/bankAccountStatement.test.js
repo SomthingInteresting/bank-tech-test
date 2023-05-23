300cb6c65e2d6e13e25be5e6bb9ab50d5410e878
Quddus Rahman <qrahman115@gmail.com>
2023-05-23 16:23:22 +0100

    adjusted feature test for statement format to be a bit more specific, limited by the changing date and asynchonous nature of jest

--- test/bankAccountStatement.test.js
+++ test/bankAccountStatement.test.js
@@ -20,4 +20,14 @@ describe('BankAccount and Statement', () => {
     expect(account.balance).toBe(500);
     expect(account.transactions.length).toBe(2);
   });
+
+  test('Statement correctly formats transactions', () => {
+    account.deposit(1000);
+    account.withdraw(500);
+
+    const statement = new Statement(account.transactions);
+    const statementOutput = statement.format();
+    expect(statementOutput).toEqual(expect.stringContaining(' || || 500.00 || 500.00'));
+    expect(statementOutput).toEqual(expect.stringContaining(' || 1000.00 || || 1000.00'));
+  });
 });
