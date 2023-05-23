0f4cfddc9298b46c2a85fc53d3d7d63df2a26892
Quddus Rahman <qrahman115@gmail.com>
2023-05-23 14:55:02 +0100

    added test for multiple transactions including different days to suite including mock adjustment to accomodate

--- test/statement.test.js
+++ test/statement.test.js
@@ -4,15 +4,19 @@ const Transaction = require('../src/transaction');
 jest.mock('../src/transaction', () => {
   let mockDate = '2023-01-10T00:00:00Z';
 
-  return jest.fn().mockImplementation((amount, type, balance) => {
+  return jest.fn().mockImplementation((amount, type, balance, date) => {
+    const transactionDate = date || mockDate;
     const transaction = {
-      datetime: new Date(mockDate),
+      datetime: new Date(transactionDate),
       amount: amount,
       type: type,
       balance: balance,
     };
 
-    mockDate = new Date(new Date(mockDate).getTime() + 1000 * 60 * 60).toISOString();
+    if (!date) {
+      mockDate = new Date(new Date(mockDate).getTime() + 1000 * 60 * 60).toISOString();
+    }
+
     return transaction;
   });
 });
@@ -50,7 +54,7 @@ describe('Statement', () => {
       expect(statement.format()).toEqual(expectedStatement);
     });
 
-    it('correctly orders and formats multiple transactions on the same day', () => {
+    test('correctly orders and formats multiple transactions on the same day', () => {
       const mockTransactions = [
         new Transaction(1000, 'credit', 1000),
         new Transaction(500, 'debit', 500),
@@ -63,6 +67,21 @@ describe('Statement', () => {
         '10/01/2023 || || 500.00 || 500.00\n' +
         '10/01/2023 || 1000.00 || || 1000.00';
       expect(statement.format()).toEqual(expectedStatement);
-    });    
+    });
+
+    test('correctly orders and formats transactions on different days', () => {
+      const mockTransactions = [
+        new Transaction(1000, 'credit', 1000, '2023-01-11T00:00:00Z'),
+        new Transaction(500, 'debit', 500),
+        new Transaction(500, 'credit', 1000, '2023-01-12T00:00:00Z'),
+      ];
+      const statement = new Statement(mockTransactions);
+      const expectedStatement = 
+        'date || credit || debit || balance\n' +
+        '12/01/2023 || 500.00 || || 1000.00\n' +
+        '11/01/2023 || 1000.00 || || 1000.00\n' +
+        '10/01/2023 || || 500.00 || 500.00';
+      expect(statement.format()).toEqual(expectedStatement);
+    });
   });
 });
